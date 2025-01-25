ea1d1cfec26327fdff9b2545db5c06debd171be1
EC2 Default User <ec2-user@ip-172-31-15-182.us-east-2.compute.internal>
2025-01-25 02:50:37 +0000

    123

---
+++ Class6-module/main.tf
@@ -0,0 +1,8 @@
+module "november-2024" {
+  source  = "Kairat-Z/november-2024/module"
+  version = "3.0.0"
+  region = "us-east-1"
+  vpc_cidr = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+  igw_name = "Kaizen"
+}
